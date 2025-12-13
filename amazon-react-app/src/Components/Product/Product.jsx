1ca219f96f078f258b60912ad97b1caa7ef8f4da
Demel Gezachew Getie <demelgizachewu@gmail.com>
2025-12-14 00:18:15 +0300

    Prelosder

--- amazon-react-app/src/Components/Product/Product.jsx
+++ amazon-react-app/src/Components/Product/Product.jsx
@@ -2,41 +2,42 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ProductCard from "./ProductCard";
 import styles from "./product.module.css";
-// import Loader from '../Loader/Loader'
+import Loader from "../Loader/Loader";
 
 function Product() {
   const [products, setProducts] = useState([]);
-  // const [isLoading,setIsLoading ]= useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get("https://fakestoreapi.com/products")
       .then((res) => {
         // console.log(res)
         setProducts(res.data);
-        // setIsLoading(false)
+        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
-        // setIsLoading(false)
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <>
-      {/* {isLoading ? (
-          <Loader />
-        ) : ( */}
-      <section className={styles.products_container}>
-        {products?.map((singleProduct) => (
-          <ProductCard
-            // renderAdd={true}
-            product={singleProduct}
-            key={singleProduct.id}
-          />
-        ))}
-      </section>
-      {/* )} */}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section className={styles.products_container}>
+          {products?.map((singleProduct) => (
+            <ProductCard
+              // renderAdd={true}
+              product={singleProduct}
+              key={singleProduct.id}
+            />
+          ))}
+        </section>
+      )}
     </>
   );
 }

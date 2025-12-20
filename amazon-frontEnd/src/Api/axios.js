364d426d17bf606d086097f003d47af6e728188a
Demel Gezachew Getie <demelgizachewu@gmail.com>
2025-12-20 17:29:34 +0300

    BackEnd deploy

--- amazon-frontEnd/src/Api/axios.js
+++ amazon-frontEnd/src/Api/axios.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const instance = axios.create({
   // local instance of firebase functions
-  baseURL: "http://127.0.0.1:5001/clone-a3913/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-a3913/us-central1/api",
   // baseURL: "http://localhost:5000",
 
   // deployed version of amazon server on render.com
-  // baseURL: "https://amazon-clone-backend-p3jr.onrender.com"
+  baseURL: "https://amazon-backend-clone.onrender.com"
 });
 
 export { instance };
